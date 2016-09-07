1d96a9c677eb422de8beca36d5a974e03a2a79d5
kaeserkiev <kaeserkiev@gmail.com>
2016-09-07 10:33:42 +0300

    today fixes

--- wp-content/themes/qmarkets/js/event.js
+++ wp-content/themes/qmarkets/js/event.js
@@ -1,5 +1,5 @@
 (function($) {
-	
+
 	$('.ev-map-item-block-btn').on('click', function(event) {
 		event.preventDefault();
 
@@ -9,13 +9,28 @@
 			$(this).addClass('clicked');
 		}
 
-		var $thisMapBlock = $(this).parents('.ev-map-item-block');
+		var $thisMapBlock = $(event.target).parents('.ev-map-item-block');
 		if( $thisMapBlock.hasClass('open')) {
 			$thisMapBlock.removeClass('open');
 		} else {
 			$thisMapBlock.addClass('open');
 		}
 
+	});
+
+	/*open map function in click on any area of map*/
+	$('.ev-map-item-outer-block').on('click', function() {
+		var $windowWidth = $(window).width();
+
+		if($windowWidth <= 1100) {
+			var $mapWraper = $(this).parent('.ev-map-item-block');
+			var $mapBtn = $(this).siblings('.ev-map-item-block-btn');
+
+			if (!$mapWraper.hasClass('open')) {
+				$mapWraper.addClass('open');
+				$mapBtn.addClass('clicked');
+			}
+		}
 	});
 
 	/*disable google map scroll*/
@@ -26,5 +41,15 @@
 		.on('mouseleave', function(){
 			$(this).find('iframe').removeClass('clicked');
 		});
+
+	/*Close map in click on any area out of map*/
+	$('.ev-map-item-block').on('click', function(e) {
+		e.stopPropagation();
+	});
+
+	$(document).on('click', function(e) {
+		$('.ev-map-item-block').removeClass('open');
+		$('.ev-map-item-block-btn').removeClass('clicked');
+	});
 
 })(jQuery);
